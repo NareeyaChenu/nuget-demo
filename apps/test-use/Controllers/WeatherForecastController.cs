9651d13973489f5f8b8582f657d4ee4ef4ba0ecd
NareeyaChenu <nachenu@gmail.com>
2022-11-11 02:15:00 +0000

    update code

--- apps/test-use/Controllers/WeatherForecastController.cs
+++ apps/test-use/Controllers/WeatherForecastController.cs
@@ -24,13 +24,15 @@ public class WeatherForecastController : ControllerBase
     [Route("forecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-       Console.WriteLine(ProductServices.GetProduct()) ;
+        Console.WriteLine(ProductServices.GetProduct()) ;
         Console.WriteLine(Calculate.CalulateMutiple(10));
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+            Name = ProductServices.GetProduct(),
+            Number = Calculate.CalulateMutiple(index)
         })
         .ToArray();
         
